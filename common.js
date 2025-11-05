429600207f728ed64c0802cdd2f70eec025d227b
Meeyana <tuanphan1112.working@gmail.com>
2025-11-05 16:19:51 +0700

    up

--- common.js
+++ common.js
@@ -171,6 +171,7 @@ function updateShareButtonState(button, message, state) {
 
 // --- Event Listeners (Chạy ngay khi file được tải) ---
 document.addEventListener('DOMContentLoaded', () => {
+    document.documentElement.classList.add('js-ready');
     // 1. Tải lịch sử ngay khi trang mở
     loadHistory();
 
