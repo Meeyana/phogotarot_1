064333d28a2256516e21f6778d78837c5c266dda
Meeyana <tuanphan1112.working@gmail.com>
2025-11-12 19:53:35 +0700

    up

--- .netlify/v1/functions/ssr/src/content/blog/ffffq.md
+++
@@ -1,8 +0,0 @@
----
-title: "ffffq"
-image: "/images/ok.jpg"
-excerpt: "vưvv"
-date: 2025-11-12T09:51:33.398Z
----
-
-svsav
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import 'dotenv/config';
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  site: 'https://phogotarot.com',
-  output: 'static', // build site tĩnh
-  adapter: netlify({
-    edge: false,
-  }),
+  output: 'server',
 });
