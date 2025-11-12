f41078627e08470124e45e1d2b9b478652270779
Meeyana <tuanphan1112.working@gmail.com>
2025-11-12 17:07:34 +0700

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://phogotarot.com',output: 'server',});
+  site: 'https://phogotarot.com',output: 'hybrid',adapter: netlify(),});
