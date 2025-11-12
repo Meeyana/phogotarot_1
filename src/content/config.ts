d390c17c1a9f32dd703e38a2d28a8fd799cdd982
Meeyana <tuanphan1112.working@gmail.com>
2025-11-12 13:20:53 +0700

    up

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+// Định nghĩa "Schema" (cấu trúc) cho bài viết blog
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    image: z.string(),
+    excerpt: z.string(),
+  }),
+});
+
+// Xuất ra bộ sưu tập tên là 'blog'
+export const collections = {
+  'blog': blogCollection,
+};
