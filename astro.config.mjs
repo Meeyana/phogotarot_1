0fd484b07fc2fdbc8518036c08ed36b4da2a84a1
Meeyana <tuanphan1112.working@gmail.com>
2025-11-12 20:21:36 +0700

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import 'dotenv/config';
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  output: 'static',
+  site: 'https://phogotarot.com',
+  output: 'static', // ✅ build toàn site thành HTML
+  adapter: netlify(), // ✅ vẫn giữ để dùng function
 });
