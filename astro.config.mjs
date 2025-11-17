e981b03762acaf601489fad0013f7eda51f02309
Meeyana <tuanphan1112.working@gmail.com>
2025-11-17 16:57:53 +0700

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import 'dotenv/config';
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://phogotarot.com',
   output: 'server', // ✅ build toàn site thành HTML
   adapter: netlify(), // ✅ vẫn giữ để dùng function
+  integrations: [
+    sitemap()
+  ]
 });
